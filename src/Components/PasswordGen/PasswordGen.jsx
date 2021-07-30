38bdd1c29d94fc6c74dac9c10151c8cc1a8186e4
kaninjegarn <propaladin@hotmail.com>
2021-07-30 10:37:41 +0200

    added checkbox logic

--- src/Components/PasswordGen/PasswordGen.jsx
+++ src/Components/PasswordGen/PasswordGen.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 const PasswordGen = () => {
   const [generatedPassword, setGeneratedPassword] = useState();
   const [passwordSize, setPasswordSize] = useState();
+  const [includeCaps, setIncludeCaps] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
 
   // Create array with the alphabet
   const alpha = Array.from(Array(26)).map((e, index) => index + 65);
@@ -24,13 +27,32 @@ const PasswordGen = () => {
 
   // Get a random value from the full array
   const handleClick = () => {
+    // TODO fix the function that checks if all true
+    const fullArr = alphabet.concat(res, numbers, otherSymbols)
+
     const random = Math.floor(Math.random() * fullArr.length);
     console.log(random, fullArr[random]);
   }
 
   // Function that will set the length of the generated passwords array.
-  const changeSize = (e) => {
-    setPasswordSize(e.target.value)
+  const changeSize = (event) => {
+    setPasswordSize(event.target.value)
+  }
+
+  // Function that handles the checkbox for caps
+  function handleCaps(event) {
+    setIncludeCaps(!includeCaps);
+    console.log(includeCaps)
+  }
+
+  function handleNumbers(event) {
+    setIncludeNumbers(!includeNumbers);
+    console.log(includeNumbers)
+  }
+
+  function handleSymbols(event) {
+    setIncludeSymbols(!includeSymbols);
+    console.log(includeSymbols)
   }
   
   return(
@@ -43,6 +65,14 @@ const PasswordGen = () => {
         <button onClick={changeSize} value={12}>12</button>
         <button onClick={changeSize} value={14}>14</button>
         <button onClick={changeSize} value={16}>16</button>
+      </div>
+      <div>
+        <label htmlFor="caps">Want both upper/lower case?</label>
+        <input type="checkbox" name="caps" value={includeCaps} checked={includeCaps} onChange={handleCaps}/>
+        <label htmlFor="numbers">Want to include numbers?</label>
+        <input type="checkbox" name="numbers" value={includeNumbers} checked={includeNumbers} onChange={handleNumbers}/>
+        <label htmlFor="otherSymbols">Want to include other symbols?</label>
+        <input type="checkbox" name="otherSymbols" value={includeSymbols} checked={includeSymbols} onChange={handleSymbols} />
       </div>
       <h1>{passwordSize}</h1>
       <button onClick={handleClick}>Click me</button>
